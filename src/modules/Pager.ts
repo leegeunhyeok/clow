54efb609d36fea6ec58c406b03cf1117dc5a8f84
leegeunhyeok <dev.ghlee@gmail.com>
2020-08-17 18:42:56 +0900

    UPDATE: Add in/output type (sample/test)

--- src/modules/Pager.ts
+++ src/modules/Pager.ts
@@ -2,11 +2,13 @@ import CrawlyModule from './common/CrawlyModule';
 import DataTypes from './common/Types';
 
 export default class PagerModule extends CrawlyModule {
+  inputType = DataTypes.STRING;
+  outputType = DataTypes.STRING;
+
   constructor(x: number = 0, y: number, color: string) {
     super();
     this.x = x;
     this.y = y;
     this.color = color;
-    this.init();
   }
 }
