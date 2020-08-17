30c66bc6f1bb5f747887659519cb1967935a0a06
leegeunhyeok <dev.ghlee@gmail.com>
2020-08-17 18:47:04 +0900

    UPDATE: Add Toolbar component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useRef, useEffect } from 'react';
 import '@svgdotjs/svg.draggable.js';
 import './App.css';
 
+import Toolbar from './components/Toolbar';
+
 import Pager from './modules/Pager';
 import CanvasContext from './CanvasContext';
 
@@ -16,12 +18,17 @@ const App = () => {
       new Pager(100, 250, '#1e90ff'),
       new Pager(250, 50, '#ffe577'),
     ];
+    modules.forEach((x) => canvasContext.registModule(x));
     const [m1, m2, m3] = modules;
     m1.connect(m2);
     m1.connect(m3);
   }, []);
 
-  return <div ref={canvas} className="App"></div>;
+  return (
+    <div ref={canvas} className="App">
+      <Toolbar />
+    </div>
+  );
 };
 
 export default App;
