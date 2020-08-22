173ec8130d7186018aa132f8eff58179da61d153
leegeunhyeok <dev.ghlee@gmail.com>
2020-08-22 21:27:12 +0900

    UPDATE: Remove moving state control

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,23 +6,17 @@ import Pager from '../modules/Pager';
 const Toolbar = () => {
   const canvasContext = CanvasContext.getInstance();
   const [connecting, setConnectingState] = useState(false);
-  const [moving, setMovingState] = useState(false);
   canvasContext.on('connectingstatechange', setConnectingState);
-  canvasContext.on('movingstatechange', setMovingState);
 
   const createModule = () => {
-    const x = Math.floor(Math.random() * 500);
-    const y = Math.floor(Math.random() * 500);
     canvasContext.registModule(new Pager(2, 3, '#aaaaaa'));
   };
 
   const toggleConnection = () => canvasContext.connecting(!connecting);
-  const toggleMoving = () => canvasContext.moving(!connecting);
 
   return (
     <div className="Toolbar">
       <button onClick={() => createModule()}>Add module</button>
-      <button onClick={() => toggleMoving()}>{moving ? 'Done' : 'Move'}</button>
       <button onClick={() => toggleConnection()}>{connecting ? 'Done' : 'Connect'}</button>
     </div>
   );
